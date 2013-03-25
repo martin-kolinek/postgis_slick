2e115f16276949d51af84c0c5fb2abf5d32e043b
Martin Kolinek <martin.kolinek@gmail.com>
2013-03-25 12:00:38 +0100

    Added some basic PostGIS functions

---
+++ src/main/scala/scala/slick/lifted/postgis/PostGISConstructorFunctions.scala
@@ -0,0 +1,8 @@
+package scala.slick.lifted.postgis
+
+import scala.slick.lifted.SimpleFunction
+import scala.slick.postgis.Geometry
+
+/*trait PostGISConstructorFunctions {
+	val makeLine = SimpleFunction.binary[Geometry, Geometry, Geometry]("ST_MakeLine")
+}*/
